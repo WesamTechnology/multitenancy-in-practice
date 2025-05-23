45dfaba6e4cf2bfb2f2a20974a07a11dcffda876
وسام تكنلوجي || Wesam Technology <wesam.technology7@gmail.com>
2025-05-24 00:22:32 +0300

    add new

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,6 +4,8 @@
         <a href="#" class="-m-1.5 p-1.5">
           <span class="sr-only">Your Company</span>
           <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+
+          <h1>wesam2</h1>
         </a>
       </div>
       <div class="flex lg:hidden">
